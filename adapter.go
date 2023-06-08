964b52c2dd91b308b3cf4c12088d52cd8aef96d9
pabapa <2301386023@qq.com>
2023-06-08 16:23:10 +0800

    fix: index

--- adapter.go
+++ adapter.go
@@ -189,7 +189,7 @@ func (a *adapter) open(clientOption *options.ClientOptions, databaseName string,
 	a.client = client
 	a.collection = collection
 
-	indexes := []string{"ptype", "v0", "v1", "v2", "v3", "v4", "v5"}
+	indexes := []string{"ptype", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"}
 	keysDoc := bsonx.Doc{}
 
 	for _, k := range indexes {
